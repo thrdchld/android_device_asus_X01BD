5b51cd50feeed5bc39a0f180a030aebc43aed0ae
bgcngm <bgcngm@gmail.com>
2020-05-11 21:02:31 +0530

    X01BD: Build lineage trust HAL
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -127,6 +127,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.asus
