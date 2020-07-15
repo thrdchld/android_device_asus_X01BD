React-Native-Noob/android_device_asus_X01BD-1

X01BD: ZenParts: Fix LED brightness preference not showing up
Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -93,6 +93,7 @@ public class DeviceSettings extends PreferenceFragment implements
 
     private CustomSeekBarPreference mTorchBrightness;
     private VibratorStrengthPreference mVibratorStrength;
+    private NotificationLedSeekBarPreference mLEDBrightness;
     private Preference mKcal;
     private SecureSettingListPreference mSPECTRUM;
     private Preference mAmbientPref;
@@ -118,12 +119,11 @@ public class DeviceSettings extends PreferenceFragment implements
 
         String device = FileUtils.getStringProp("ro.build.product", "unknown");
 
-        if (FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) && FileUtils.fileWritable(NOTIF_LED_RED_PATH) && FileUtils.fileWritable(NOTIF_LED_GREEN_PATH)) {
-            CustomSeekBarPreference notifLedBrightness =
-                    (CustomSeekBarPreference) findPreference(PREF_NOTIF_LED);
-            notifLedBrightness.setOnPreferenceChangeListener(this);
-        } else { getPreferenceScreen().removePreference(findPreference(CATEGORY_NOTIF)); }
-
+        mLEDBrightness = (CustomSeekBarPreference) findPreference(CATEGORY_NOTIF);
+        mLEDBrightness.setEnabled(FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) &&
+              FileUtils.fileWritable(NOTIF_LED_RED_PATH) &&
+                  FileUtils.fileWritable(NOTIF_LED_GREEN_PATH));
+        mLEDBrightness.setOnPreferenceChangeListener(this);
 
         mTorchBrightness = (CustomSeekBarPreference) findPreference(PREF_TORCH_BRIGHTNESS);
         mTorchBrightness.setEnabled(FileUtils.fileWritable(TORCH_1_BRIGHTNESS_PATH) &&

thrdchld/android_device_asus_X01BD

ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -94,6 +94,7 @@ public class DeviceSettings extends PreferenceFragment implements
 
     private CustomSeekBarPreference mTorchBrightness;
     private VibratorStrengthPreference mVibratorStrength;
+    private NotificationLedSeekBarPreference mLEDBrightness;
     private Preference mKcal;
     private SecureSettingListPreference mSPECTRUM;
     private Preference mAmbientPref;
@@ -119,12 +120,11 @@ public class DeviceSettings extends PreferenceFragment implements
 
         String device = FileUtils.getStringProp("ro.build.product", "unknown");
 
-        if (FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) && FileUtils.fileWritable(NOTIF_LED_RED_PATH) && FileUtils.fileWritable(NOTIF_LED_GREEN_PATH)) {
-            NotificationLedSeekBarPreference notifLedBrightness =
-                    (NotificationLedSeekBarPreference) findPreference(PREF_NOTIF_LED);
-            notifLedBrightness.setOnPreferenceChangeListener(this);
-        } else { getPreferenceScreen().removePreference(findPreference(CATEGORY_NOTIF)); }
-
+        mLEDBrightness = (NotificationLedSeekBarPreference) findPreference(PREF_NOTIF_LED);
+        mLEDBrightness.setEnabled(FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) &&
+              FileUtils.fileWritable(NOTIF_LED_RED_PATH) &&
+                  FileUtils.fileWritable(NOTIF_LED_GREEN_PATH));
+        mLEDBrightness.setOnPreferenceChangeListener(this);
 
         mTorchBrightness = (CustomSeekBarPreference) findPreference(PREF_TORCH_BRIGHTNESS);
         mTorchBrightness.setEnabled(FileUtils.fileWritable(TORCH_1_BRIGHTNESS_PATH) &&