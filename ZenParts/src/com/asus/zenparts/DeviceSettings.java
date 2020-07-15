React-Native-Noob/android_device_asus_X01BD-1

X01BD: ZenParts: Add notification LED brightness slider
* Adapted to work with RGB Leds by @SonalSingh18

Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -80,6 +80,12 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String PREF_MSM_TOUCHBOOST = "touchboost";
     public static final String MSM_TOUCHBOOST_PATH = "/sys/module/msm_performance/parameters/touchboost";
 
+    public static final String CATEGORY_NOTIF = "notification_led";
+    public static final String PREF_NOTIF_LED = "notification_led_brightness";
+    public static final String NOTIF_LED_BLUE_PATH = "/sys/class/leds/blue/max_brightness";
+    public static final String NOTIF_LED_RED_PATH = "/sys/class/leds/red/max_brightness";
+    public static final String NOTIF_LED_GREEN_PATH = "/sys/class/leds/green/max_brightness";
+
     private CustomSeekBarPreference mTorchBrightness;
     private VibratorStrengthPreference mVibratorStrength;
     private Preference mKcal;
@@ -106,6 +112,13 @@ public class DeviceSettings extends PreferenceFragment implements
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
 
         String device = FileUtils.getStringProp("ro.build.product", "unknown");
+
+        if (FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) && FileUtils.fileWritable(NOTIF_LED_RED_PATH) && FileUtils.fileWritable(NOTIF_LED_GREEN_PATH)) {
+            CustomSeekBarPreference notifLedBrightness =
+                    (CustomSeekBarPreference) findPreference(PREF_NOTIF_LED);
+            notifLedBrightness.setOnPreferenceChangeListener(this);
+        } else { getPreferenceScreen().removePreference(findPreference(CATEGORY_NOTIF)); }
+
 
         mTorchBrightness = (CustomSeekBarPreference) findPreference(PREF_TORCH_BRIGHTNESS);
         mTorchBrightness.setEnabled(FileUtils.fileWritable(TORCH_1_BRIGHTNESS_PATH) &&
@@ -298,6 +311,12 @@ public class DeviceSettings extends PreferenceFragment implements
                 mCPUBOOST.setValue((String) value);
                 mCPUBOOST.setSummary(mCPUBOOST.getEntry());
                 FileUtils.setStringProp(CPUBOOST_SYSTEM_PROPERTY, (String) value);
+                break;
+
+            case PREF_NOTIF_LED:
+                FileUtils.setValue(NOTIF_LED_BLUE_PATH, (int) value);
+                FileUtils.setValue(NOTIF_LED_RED_PATH, (int) value);
+                FileUtils.setValue(NOTIF_LED_GREEN_PATH, (int) value);
                 break;
 
             default:

thrdchld/android_device_asus_X01BD

ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -34,6 +34,7 @@ import com.asus.zenparts.preferences.CustomSeekBarPreference;
 import com.asus.zenparts.preferences.SecureSettingListPreference;
 import com.asus.zenparts.preferences.SecureSettingSwitchPreference;
 import com.asus.zenparts.preferences.VibratorStrengthPreference;
+import com.asus.zenparts.preferences.NotificationLedSeekBarPreference;
 
 public class DeviceSettings extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -80,6 +81,12 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String PREF_MSM_TOUCHBOOST = "touchboost";
     public static final String MSM_TOUCHBOOST_PATH = "/sys/module/msm_performance/parameters/touchboost";
 
+    public static final String CATEGORY_NOTIF = "notification_led";
+    public static final String PREF_NOTIF_LED = "notification_led_brightness";
+    public static final String NOTIF_LED_BLUE_PATH = "/sys/class/leds/blue/max_brightness";
+    public static final String NOTIF_LED_RED_PATH = "/sys/class/leds/red/max_brightness";
+    public static final String NOTIF_LED_GREEN_PATH = "/sys/class/leds/green/max_brightness";
+
     private CustomSeekBarPreference mTorchBrightness;
     private VibratorStrengthPreference mVibratorStrength;
     private Preference mKcal;
@@ -106,6 +113,13 @@ public class DeviceSettings extends PreferenceFragment implements
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
 
         String device = FileUtils.getStringProp("ro.build.product", "unknown");
+
+        if (FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) && FileUtils.fileWritable(NOTIF_LED_RED_PATH) && FileUtils.fileWritable(NOTIF_LED_GREEN_PATH)) {
+            NotificationLedSeekBarPreference notifLedBrightness =
+                    (NotificationLedSeekBarPreference) findPreference(PREF_NOTIF_LED);
+            notifLedBrightness.setOnPreferenceChangeListener(this);
+        } else { getPreferenceScreen().removePreference(findPreference(CATEGORY_NOTIF)); }
+
 
         mTorchBrightness = (CustomSeekBarPreference) findPreference(PREF_TORCH_BRIGHTNESS);
         mTorchBrightness.setEnabled(FileUtils.fileWritable(TORCH_1_BRIGHTNESS_PATH) &&
@@ -298,6 +312,12 @@ public class DeviceSettings extends PreferenceFragment implements
                 mCPUBOOST.setValue((String) value);
                 mCPUBOOST.setSummary(mCPUBOOST.getEntry());
                 FileUtils.setStringProp(CPUBOOST_SYSTEM_PROPERTY, (String) value);
+                break;
+
+            case PREF_NOTIF_LED:
+                FileUtils.setValue(NOTIF_LED_BLUE_PATH, (int) value);
+                FileUtils.setValue(NOTIF_LED_RED_PATH, (int) value);
+                FileUtils.setValue(NOTIF_LED_GREEN_PATH, (int) value);
                 break;
 
             default: