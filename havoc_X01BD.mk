d5442ec4a80d65c6fbc22197d4bd20017197ebc7
Sonal Singh <sonal.singh.19993@gmail.com>
2021-06-19 13:52:25 +0530

    X01BD: arrow => havoc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- havoc_X01BD.mk
+++ havoc_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 ArrowOS
+# Copyright (C) 2020 Havoc-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,16 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Havoc-OS stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-DEVICE_MAINTAINER := SonalSingh
+HAVOC_BUILD_TYPE := Official
+HAVOC_MAINTAINER := SonalSingh
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_X01BD
+PRODUCT_NAME := havoc_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Asus Zenfone Max Pro M2
