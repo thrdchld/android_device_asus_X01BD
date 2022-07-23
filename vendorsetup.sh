49727a9c1deedc9d20e09e07af4746446b5a8b30
Sonal Singh <sonal.singh.19993@gmail.com>
2022-07-23 10:27:59 +0000

    X01BD: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ArrowOS-Devices/android_device_asus_sdm660-common.git -b arrow-12.1 device/asus/sdm660-common
+git clone https://github.com/ArrowOS-Devices/android_vendor_asus_sdm660.git -b arrow-12.1 vendor/asus
+git clone https://github.com/ArrowOS-Devices/android_kernel_asus_X01BD.git -b arrow-12.0_eas kernel/asus/sdm660
+git clone https://github.com/SonalSingh18/android_vendor_lawnchair.git -b arrow-12.1 vendor/lawnchair
+git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.1 packages/apps/GCamGOPrebuilt
+git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b.git -b master prebuilts/clang/host/linux-x86/clang-r437112b
