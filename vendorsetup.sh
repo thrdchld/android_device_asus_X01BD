5a48fc001cf88a5163238c02522f9efa3b079667
Sonal Singh <sonal.singh.19993@gmail.com>
2022-07-23 10:27:59 +0000

    X01BD: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/SonalSingh18/android_device_asus_sdm660-common.git -b arrow-13.0 device/asus/sdm660-common
+git clone https://github.com/SonalSingh18/android_vendor_asus_sdm660.git -b arrow-13.0 vendor/asus
+git clone https://github.com/SonalSingh18/android_kernel_asus_sdm660.git -b arrow-13.0 kernel/asus/sdm660
+git clone https://github.com/LineageOS/android_hardware_qcom_vr.git -b lineage-20.0 hardware/qcom-caf/vr
+git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt.git -b arrow-13.0 packages/apps/GCamGOPrebuilt
+git clone https://gitlab.com/GhostMaster69-dev/cosmic-clang.git -b master prebuilts/clang/host/linux-x86/clang-cosmic
