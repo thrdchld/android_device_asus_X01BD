54b00919126ed8b9661e422d4031d012e612ccfd
thrd chld <thirdchilddesigner@gmail.com>
2024-06-23 14:08:08 +0000

    lineage --> qassa

--- device.mk
+++ device.mk
@@ -269,7 +269,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-qassa
 
 # IPv6
 PRODUCT_PACKAGES += \
